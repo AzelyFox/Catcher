6c0532aff86664f100ed78fc32cc56c472206118
LimeCake <limecake23@gmail.com>
2020-04-22 13:39:45 +0900

    Android update

--- Android/catcher/src/main/java/kr/devx/catcher/Catcher.java
+++ Android/catcher/src/main/java/kr/devx/catcher/Catcher.java
@@ -2,15 +2,12 @@ package kr.devx.catcher;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.os.AsyncTask;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Catcher {
-    public static void newLogGUI(Activity activity, String key, double marketMoveMinRate, String marketPackage) {
-
-    }
-
     public static void newLog(String key, Log log) {
         ContentValues values = new ContentValues();
         values.put("service_key", key);
@@ -36,8 +33,13 @@ public class Catcher {
         protected String doInBackground(Void... params) {
 
             String result;
-            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            result = requestHttpURLConnection.request(url, values);
+            try {
+                RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+                result = requestHttpURLConnection.request(url, values);
+            } catch (Exception e) {
+                result = null;
+                android.util.Log.e("DEVX-CATCHER","ERROR : " + e.getCause());
+            }
 
             return result;
         }
@@ -45,24 +47,17 @@ public class Catcher {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            if (s == null) {
+                return;
+            }
             try {
                 JSONObject result = new JSONObject(s);
                 int resultCode = result.getInt("result");
                 if (resultCode == 0) {
                     android.util.Log.i("DEVX-CATCHER","SUCCESS : LOG ADDED : " + result.getInt("log_index"));
                 } else {
-                    if (resultCode == -1) {
-                        android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error"));
-                        android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error_debug"));
-                    }
-                    if (resultCode == -2) {
-                        android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error"));
-                        android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error_debug"));
-                    }
-                    if (resultCode == -3) {
-                        android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error"));
-                        android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error_debug"));
-                    }
+                    android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error"));
+                    android.util.Log.e("DEVX-CATCHER","FAIL : " + result.getInt("error_debug"));
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
